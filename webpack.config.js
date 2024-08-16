a42e952f05e9d2a1c6076e64951460f1d1db5221
GabrielDevJourney <gabspereira178@gmail.com>
2024-08-16 09:03:23 +0100

    inicial config created and understood some core things about webpack to run things nice

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+		}),
+	],
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "dist"),
+		},
+		compress: true,
+		port: 'auto',
+        open: true,
+	},
+};
