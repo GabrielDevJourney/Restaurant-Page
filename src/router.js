e39f3b63d33d239580948c253e96ce80527dc934
GabrielDevJourney <gabspereira178@gmail.com>
2024-08-23 08:36:21 +0100

    creating a rounting file to update the current page hash the active link in header and the main content of the page that is not yet implemented

---
+++ src/router.js
@@ -0,0 +1,40 @@
+import { renderHomePage } from "./pages/Home.js";
+import { renderMenuPage } from "./pages/Menu.js";
+import { renderAboutPage } from "./pages/About.js";
+
+const routes = {
+	home: renderHomePage,
+	menu: renderMenuPage,
+	about: renderAboutPage,
+};
+
+export function initRouter(header) {
+	window.navigateTo = (page) => {
+		console.log("Navigating to:", page); // Add this debug log
+		const normalizedPage = page.toLowerCase();
+		window.location.hash = normalizedPage;
+		header.updateActiveLink(normalizedPage);
+		updatePageContent(normalizedPage);
+	};
+
+	// Handle initial page load and browser back/forward
+	window.addEventListener("hashchange", () => {
+		const page = window.location.hash.slice(1) || "home";
+		navigateTo(page);
+	});
+
+	const inicialPage = window.location.hash.slice(1) || "home";
+
+	// Handle the initial page load
+	navigateTo(inicialPage);
+}
+function updatePageContent(page) {
+	const contentDiv = document.getElementById("content");
+	contentDiv.innerHTML = ""; // Clear current content
+
+	if (routes[page]) {
+		contentDiv.appendChild(routes[page]());
+	} else {
+		contentDiv.textContent = "404 - Page not found";
+	}
+}
