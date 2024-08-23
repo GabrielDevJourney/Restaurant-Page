369ce574c078cfe79ffa40f73f0a7a75f2f186f5
GabrielDevJourney <gabspereira178@gmail.com>
2024-08-23 08:38:56 +0100

    changed from fucntion to class for better maintance and practice since this component is a dynamic one, also implemented updating the styling of each nav link when clicked and changed pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,75 @@
-import cartIconUrl from '../assets/cartIcon.png'
-export function createHeader(){
-    const headerContainer = document.createElement('div')
-    headerContainer.className = 'headerContainer'
+import cartIconUrl from "../assets/cartIcon.png";
+export class Header {
+	constructor() {
+		this.pages = ["Home", "Menu", "About"];
+		this.element = this.createHeader();
+		this.navLinks = this.element.querySelectorAll(".navContainer a");
+	}
+	createHeader() {
+		const headerContainer = document.createElement("div");
+		headerContainer.className = "headerContainer";
 
-    //Logo text container
-    const logoTextContainer = document.createElement('div')
-    logoTextContainer.className = 'logoTextContainer'
-    const logoText = document.createElement('p')
-    logoText.className = 'logoText'
-    logoText.textContent = 'KAIZEN HOUSE'
-    logoTextContainer.appendChild(logoText)
-    headerContainer.appendChild(logoTextContainer)
+		//Logo text container
+		const logoTextContainer = document.createElement("div");
+		logoTextContainer.className = "logoTextContainer";
+		const logoText = document.createElement("p");
+		logoText.className = "logoText";
+		logoText.textContent = "KAIZEN HOUSE";
+		logoTextContainer.appendChild(logoText);
+		headerContainer.appendChild(logoTextContainer);
 
-    //NAV CONTAINER
-    const navContainer = document.createElement('div')
-    navContainer.className = 'navContainer'
-    const pages = ['Home', 'Menu', 'About']
-    pages.forEach(page => {
-        const a = document.createElement('a')
-        a.href = '#'
-        a.textContent = page
-        // i learned this today(19/08/24) so this will create a data*(data-page) attribute and will set it to the page name in lowercase known as a best practice since i will reuse the value for routing pages
-        a.dataset.page = page.toLowerCase()
-        a.addEventListener('click', (e) => {
-            e.preventDefault()
-            //TODO
-            //IMPLEMENT NAVIGATETO
-            window.navigateTo(e.target.dataset.page)
+		//NAV CONTAINER
+		const navContainer = document.createElement("div");
+		navContainer.className = "navContainer";
+		this.pages.forEach((page) => {
+			const a = document.createElement("a");
+			a.href = "#" + page.toLocaleLowerCase();
+			a.textContent = page;
+			// i learned this today(19/08/24) so this will create a data*(data-page) attribute and will set it to the page name in lowercase known as a best practice since i will reuse the value for routing pages
+			a.dataset.page = page.toLowerCase();
+            a.addEventListener("click", (e) => {
+				e.preventDefault();
+				console.log("Link clicked:", e.target.dataset.page); // Add this debug log
+				window.navigateTo(e.target.dataset.page);
+			});
+			navContainer.appendChild(a);
+		});
 
-        })
-        navContainer.appendChild(a)
-    })
+		headerContainer.appendChild(navContainer);
 
-    headerContainer.appendChild(navContainer)
+		//ICON CONTAINER
+		const iconContainer = document.createElement("div");
+		iconContainer.className = "iconContainer";
+		const cartIcon = document.createElement("img");
+		cartIcon.className = "cartIcon";
+		cartIcon.src = cartIconUrl;
+		cartIcon.alt = "Cart";
+		const cartItemCount = document.createElement("span");
+		cartItemCount.className = "cartCounter";
+		cartItemCount.textContent = "0";
+		iconContainer.appendChild(cartIcon);
+		iconContainer.appendChild(cartItemCount);
+		headerContainer.appendChild(iconContainer);
 
+		return headerContainer;
+	}
 
-    //ICON CONTAINER
-    const iconContainer = document.createElement('div')
-    iconContainer.className = 'iconContainer'
-    const cartIcon = document.createElement('img')
-    cartIcon.className = 'cartIcon'
-    cartIcon.src = cartIconUrl
-    cartIcon.alt = "Cart"
-    const cartItemCount = document.createElement('span')
-    cartItemCount.className = 'cartCounter'
-    cartItemCount.textContent = '0'
-    iconContainer.appendChild(cartIcon)
-    iconContainer.appendChild(cartItemCount)
-    headerContainer.appendChild(iconContainer)
+	get currentPage() {
+		const hash = window.location.hash.slice(1);
+		return hash || "home";
+	}
 
-    return headerContainer
+	updateActiveLink(activePage) {
+		this.navLinks.forEach((link) => {
+			if (link.dataset.page === activePage) {
+				link.classList.add("activeLink");
+			} else {
+				link.classList.remove("activeLink");
+			}
+		});
+	}
+
+	getElement() {
+		return this.element;
+	}
 }
-
